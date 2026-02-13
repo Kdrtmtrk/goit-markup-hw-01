301b9e4665a57784a842f3eff480c81d163c089e
kadir_tumturk <kdrtumturk98@gmail.com>
2026-02-13 21:18:36 +0300

    update

--- script.js
+++ script.js
@@ -63,6 +63,9 @@ const messages = [
     "Özür dilerim hadi tamam bas artik evete ❤️",
     "İşi senin için kolaylaştirayim.",
     "Şakaydi ama bas hadi artik evete! ❤️"
+    "Seni seviyorum bas arik lütfen."
+    "Son 2 şansin yeter"
+    "Beni seviyosan basarsin evete ❤️"
 ];
 
 let messageIndex = 0;
